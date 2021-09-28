b6ae8c10125fbd08214c5a0c869a53c1ce19fb50
prsn-ts <turbo6040@gmail.com>
2021-09-29 00:04:27 +0900

    입문 003번 따라해보기

--- src/App.js
+++ src/App.js
@@ -1,28 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import ImportConponent from './R003_ImportConponent'
 
 function App() {
   return (
     <div>
       <h1>Start React 200!</h1>
       <p>HTML 적용하기</p>
+      {/* 
+          아래에 ImportConponent 태그가 컴포넌트가 사용되는 부분
+       */}
+      <ImportConponent></ImportConponent>
     </div>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
   );
 }
 
