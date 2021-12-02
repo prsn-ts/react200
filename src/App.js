f215360623b5209bc51fe544b6b5fac8f8ce858c
prsn-ts <turbo6040@gmail.com>
2021-12-02 21:06:29 +0900

    생명주기 함수 getDerivedStateFromProps(props, state)에 대해(입문 006)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import LifecycleEx from './R005_LifecycleEx'
+import LifecycleEx from './R006_LifecycleEx'
 
 function App() {
   return (
@@ -9,8 +9,9 @@ function App() {
       <p>HTML 적용하기</p>
       {/* 
           아래에 LifecycleEx 태그가 컴포넌트가 사용되는 부분
+          LifecycleEx 컴포넌트에 'FromApp.js'라는 값을 가지는 prop_value 변수를 전달한다. 
        */}
-      <LifecycleEx></LifecycleEx>
+      <LifecycleEx prop_value = 'FromApp.js'/>
     </div>
   );
 }
