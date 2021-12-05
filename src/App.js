24ab77568cdb7732463f312284be7a53dd685756
prsn-ts <turbo6040@gmail.com>
2021-12-05 16:10:06 +0900

    ES6의 템플릿 문자열(Template Literal)에 대해(입문 009)

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import './App.css';
-import LifecycleEx from './R008_LifecycleEx'
+import Es6 from './R009_Es6'
 
 function App() {
   return (
     <div>
       <h1>Start React 200!</h1>
       <p>HTML 적용하기</p>
-      {/* 
-          아래에 LifecycleEx 태그가 컴포넌트가 사용되는 부분
-          LifecycleEx 컴포넌트에 'FromApp.js'라는 값을 가지는 prop_value 변수를 전달한다. 
-       */}
-      <LifecycleEx prop_value = 'FromApp.js'/>
+      <Es6/>
     </div>
   );
 }
