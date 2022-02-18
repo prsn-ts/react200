8465a92e38efeddfb4443a7029034c4037492394
prsn-ts <turbo6040@gmail.com>
2022-02-18 22:01:31 +0900

    reactstrap Card 사용하기(초급 040)

---
+++ src/R040_ReactstrapCard.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { 
+    Card, CardImg, CardText, CardBody,
+    CardTitle, CardSubtitle, Button
+} from 'reactstrap';
+
+class R040_ReactstrapCard extends Component {
+    render() {
+        return (
+            <div>
+                <Card>
+                    <CardImg top height='200px' src='/image/landscape.jpg' alt='Card image'/>
+                    <CardBody>
+                        <CardTitle>Card 제목</CardTitle>
+                        <CardSubtitle>Card 부제목</CardSubtitle>
+                        <CardText>Card 내용 Lorem Ipsum is simply dummy text.</CardText>
+                        <Button>버튼</Button>
+                    </CardBody>
+                </Card>
+            </div>
+        )
+    }
+}
+
+export default R040_ReactstrapCard;
