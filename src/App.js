5e50c4a94bc1502a26ce612a94f67d9b1093fce2
prsn-ts <turbo6040@gmail.com>
2022-02-25 06:58:27 +0900

    reactstrap List Group사용하기(초급 047)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactstrapJumbotron from './R046_ReactstrapJumbotron'
+import ListGroup from './R047_ReactstrapListGroup'
 import 'bootstrap/dist/css/bootstrap.css'
 
 function App() {
@@ -7,7 +7,7 @@ function App() {
     <div>
       <h1>Start React 200!</h1>
       <p>CSS 적용하기</p>
-      <ReactstrapJumbotron/>
+      <ListGroup/>
     </div>
   );
 }
---
+++ src/R047_ReactstrapListGroup.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { ListGroup, ListGroupItem, Badge } from 'reactstrap';
+
+class R047_ReactstrapListGroup extends Component {
+    render() {
+        return (
+            <>
+                <ListGroup>
+                    <ListGroupItem color='danger'
+                    className='justify-content-between'>Badge
+                        <Badge pill>200</Badge>
+                    </ListGroupItem>
+                    <ListGroupItem disabled tag='a' href='https://www.naver.com'>
+                    List</ListGroupItem>
+                    <ListGroupItem tag='button' action onClick={e => alert("button")}>Button</ListGroupItem>
+                </ListGroup>
+            </>
+        )
+    }
+}
+
+export default R047_ReactstrapListGroup;
