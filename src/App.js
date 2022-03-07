38c36814e32226a7e87da85367dc615adb628b44
prsn-ts <turbo6040@gmail.com>
2022-03-08 06:54:05 +0900

    처리하기 전에 확인하는 알림 창을 위한 sweetalert2 Confirm 사용하기(초급 058)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import Sweetalert2Position from './R057_Sweetalert2Position'
+import Sweetalert2Confirm from './R058_Sweetalert2Confirm'
 import 'bootstrap/dist/css/bootstrap.css'
 
 function App() {
   return (
     <div>
       <h1>Start React 200!</h1>
-      <Sweetalert2Position/>
+      <Sweetalert2Confirm/>
     </div>
   );
 }
