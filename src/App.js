2c01f250942424faab194f02b83174415b9d0b30
prsn-ts <turbo6040@gmail.com>
2022-03-29 09:00:00 +0900

    redux 스토어 상속과 디스패치 함수 실행하기(중급 079)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import ContextApi from './Context/R077_ContextApi'
+import React, {Component} from 'react'
+import StrAddButton from './StrAddButton'
 
-function App() {
-  return (
-    <div>
-      <h1>Start React 200!</h1>
-      <ContextApi/>
-    </div>
-  );
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Start React 200!</h1>
+                <span>{this.props.store.getState().data.str}</span><br/>
+                <StrAddButton store={this.props.store}></StrAddButton>
+            </div>
+        );
+    }
 }
 
 export default App;
