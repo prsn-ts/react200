9efb86e388a4df06ace461af0271cb1000519f83
prsn-ts <turbo6040@gmail.com>
2022-04-01 09:00:00 +0900

    react-redux로 스토어 데이터 사용하기(중급 082)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import {connect} from 'react-redux'
 import StrAddButton from './StrAddButton'
 
 class App extends Component {
@@ -6,11 +7,22 @@ class App extends Component {
         return (
             <div>
                 <h1>Start React 200!</h1>
-                <span>{this.props.store.getState().data.str}</span><br/>
-                <StrAddButton store={this.props.store}></StrAddButton>
+                {/* <span>{this.props.store.getState().data.str}</span><br/> */}
+                <span>{this.props.str}</span>
+                {/* <StrAddButton store={this.props.store}></StrAddButton> */}
+                <StrAddButton AppProp='200'/>
             </div>
         );
     }
 }
+
+let mapStateToProps = (state, props) => {
+    console.log('Props from index.js : ' + props.indexProp)
+    return {
+        str: state.data.str,
+    }
+};
+
+App = connect(mapStateToProps, null)(App);
 
 export default App;
