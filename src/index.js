cc5bb803bc97a5e43bd2e8e06282484ab301b6c1
prsn-ts <turbo6040@gmail.com>
2022-04-07 20:36:09 +0900

    react-router-dom BrowserRouter 사용하기(중급 088)

--- src/index.js
+++ src/index.js
@@ -1,39 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
-import {createStore, applyMiddleware} from 'redux';
-import {Provider} from 'react-redux';
-import reducers from './reducers';
+import * as serviceWorker from './serviceWorker';
 
-const CallMiddleware = store => nextMiddle => action => {
-    console.log('1. 첫 번째 미들웨어 reducer 실행 전');
-    console.log('2. 첫 번째 미들웨어 action.type : '+action.type+', store str : '+store.getState().data.str);
-    let result = nextMiddle(action);
-    console.log('7. 첫 번째 미들웨어 reducer 실행 후');
-    console.log('8. 첫 번째 미들웨어 action.type : '+action.type+', store str : '+store.getState().data.str);
-    return result;
-}
+ReactDOM.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>,
+    document.getElementById('root')
+);
 
-const CallMiddleware2 = store => nextMiddle => action => {
-    console.log('3. 두 번째 미들웨어 reducer 실행 전');
-    console.log('4. 두 번째 미들웨어 action.type : '+action.type+', store str : '+store.getState().data.str);
-    let result = nextMiddle(action);
-    console.log('5. 두 번째 미들웨어 reducer 실행 후');
-    console.log('6. 두 번째 미들웨어 action.type : '+action.type+', store str : '+store.getState().data.str);
-    return result;
-}
-
-const store = createStore(reducers, applyMiddleware(CallMiddleware, CallMiddleware2));
-
-const listener = ()=> {
-    ReactDOM.render(
-        <Provider store={store}>
-            <App indexProp='react'/>
-        </Provider>,
-        document.getElementById('root')
-    );
-}
-
-store.subscribe(listener);
-listener();
+serviceWorker.unregister();
