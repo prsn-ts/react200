acdb247524ac9fb36d4d0f106a48bca2333147d1
prsn-ts <turbo6040@gmail.com>
2022-04-10 14:22:29 +0900

    header, footer 구현하기(중급 091)

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,25 @@ import { Route, Routes } from 'react-router-dom';
 import ReactRouter from './R089_reactRouter';
 import ReactRouter2 from './R089_reactRouter2';
 
+// css
+import '../css/new.css';
+
+// header
+import HeaderAdmin from './Header/Header admin';
+
+// footer
+import Footer from './Footer/Footer';
+
 class App extends Component {
     render() {
         return (
             <div className='App'>
+                <HeaderAdmin />
                 <Routes>
                     <Route path="/" element={<ReactRouter />} />
                     <Route path="/reactRouter2" element={<ReactRouter2 />} />
                 </Routes>
+                <Footer />
             </div>
         )
     }
