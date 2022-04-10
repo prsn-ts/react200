cec65f40d3f533b383a077ee99f1121c8e79ce2f
prsn-ts <turbo6040@gmail.com>
2022-04-11 06:43:34 +0900

    home 페이지 만들기(중급 092)

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import ReactRouter from './R089_reactRouter';
-import ReactRouter2 from './R089_reactRouter2';
+// import ReactRouter from './R089_reactRouter';
+// import ReactRouter2 from './R089_reactRouter2';
 
 // css
 import '../css/new.css';
@@ -12,14 +12,16 @@ import HeaderAdmin from './Header/Header admin';
 // footer
 import Footer from './Footer/Footer';
 
+// login
+import LoginForm from './LoginForm';
+
 class App extends Component {
     render() {
         return (
             <div className='App'>
                 <HeaderAdmin />
                 <Routes>
-                    <Route path="/" element={<ReactRouter />} />
-                    <Route path="/reactRouter2" element={<ReactRouter2 />} />
+                    <Route path="/" element={<LoginForm />} />
                 </Routes>
                 <Footer />
             </div>
